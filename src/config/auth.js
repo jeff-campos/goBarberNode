d789ec790ba759e2f12584b1ccf385151f3196f8
Jefferson Jarbas De Oliveira Campos <jcampo8@brrva1l10674018.nextel.com.br>
2019-08-31 12:28:53 -0300

    feature(initial): start project

---
+++ src/config/auth.js
@@ -0,0 +1,4 @@
+export default {
+  secret: '1a4ed1d1510a5ed2f15c2dd11c0e18de',
+  expiresIn: '7d',
+};
---
+++ src/database/migrations/20190830193538-create-users.js
@@ -0,0 +1,46 @@
+module.exports = {
+  up: (queryInterface, Sequelize) => queryInterface.createTable('users', {
+    id: {
+      type: Sequelize.INTEGER, // Tipo da variavél é INTEGER
+      allowNull: false, // Não permite valor nulo
+      autoIncrement: true, // Auto Incremental
+      primaryKey: true, // Chave Primaria da tabela
+    },
+    name: {
+      type: Sequelize.STRING,
+      allowNull: false,
+    },
+    email: {
+      type: Sequelize.STRING, // Tipo Sting
+      allowNull: false,
+      unique: true,
+    },
+    password_hash: {
+      /* esse _hash indica que não vamos armazenar a senha
+        omo foi criada, ex: 123, vamos criptografar, sendo assim, armazenaremos um hash dela
+        */
+      type: Sequelize.STRING,
+      allowNull: false,
+    },
+    provider: {
+      /* essa aplicação agendamento de serviços de beleza, portanto
+      permitirá usuários CLIENTES e usuários PRESTADORES de serviço, quando o usuário por
+      prestador de serviço o tipo provider é TRUE, por padrão ele é falso o que
+      indica que o usuário é um Cliente.
+        */
+      type: Sequelize.BOOLEAN,
+      defaultValue: false,
+      allowNull: false,
+    },
+    created_at: { // data de criação do registro
+      type: Sequelize.DATE,
+      allowNull: false,
+    },
+    updated_at: { // data de atualização dop registro
+      type: Sequelize.DATE,
+      allowNull: false,
+    },
+  }),
+
+  down: (queryInterface) => queryInterface.dropTable('users'),
+};
