0eb3e901fa9b5b69aac0fe6c7e93a026435d9ce2
Admin <admin@Milenes-MBP.lan>
2019-11-16 18:19:12 -0300

    feat(user|avatar): inserting model, controller and router for image file

--- src/routes.js
+++ src/routes.js
@@ -1,15 +1,24 @@
 import { Router } from 'express';
+import multer from 'multer';
+import multerConfig from './config/multer';
+
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
+import FileController from './app/controllers/FileController';
+import ProvidersController from './app/controllers/ProvidersController';
+
 import authMiddlewere from './app/middlewares/auth';
 
 const routes = new Router();
+const upload = multer(multerConfig);
 
 routes.post('/users', UserController.store);
 routes.post('/sessions', SessionController.store);
 
 routes.use(authMiddlewere);
+routes.get('/providers', ProvidersController.index);
 routes.put('/users', UserController.update);
+routes.post('/files', upload.single('file'), FileController.store);
 
 
 export default routes;
